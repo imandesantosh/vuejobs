e3c8aea71821b4ff5e77682dabd9413777dde4b9
santoshande <mrsantoshande@gmail.com>
2026-06-07 10:51:33 +0530

    update url

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://restapi-znr9.onrender.com',
+        target: 'https://restapi-znr9.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
